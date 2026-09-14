694c0aaa8286bc403ea6f4d92a42747969b29b89
myselfrxvi <nooblikemeyo@gmail.com>
2026-09-14 16:34:54 +0530

    feat: add LeetCode solutions for trapping rain water, multiply strings, and rectangle overlap with updated README count

---
+++ leetcode/strings/43_multiply_strings.py
@@ -0,0 +1,16 @@
+class Solution:
+    def multiply(self, num1: str, num2: str) -> str:
+        if num1 == "0" or num2 == "0":
+            return "0"
+
+        m, n = len(num1), len(num2)
+        res = [0] * (m + n)
+
+        for i in range(m - 1, -1, -1):
+            for j in range(n - 1, -1, -1):
+                p1 = i + j 
+                p2 = i + j + 1
+                mul = int(num1[i]) * int(num2[j]) + res[p2]
+                res[p2] = mul % 10
+                res[p1] += mul // 10
+        return "".join(map(str, res)).lstrip("0")
