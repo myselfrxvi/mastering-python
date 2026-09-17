a4afe61938ccc65ec2c5c609de2fa4a5ae7733b0
myselfrxvi <nooblikemeyo@gmail.com>
2026-09-17 15:41:27 +0530

    Update LC #60 with user implementation

--- leetcode/math_and_bits/60_permutation_sequence.py
+++ leetcode/math_and_bits/60_permutation_sequence.py
@@ -3,14 +3,11 @@ class Solution:
         factorials = [1] * n
         for i in range(1, n):
             factorials[i] = factorials[i - 1] * i
-
         numbers = list(range(1, n + 1))
-        k -= 1
         res = []
-
+        k -= 1
         for i in range(n - 1, -1, -1):
             idx = k // factorials[i]
             k %= factorials[i]
             res.append(str(numbers.pop(idx)))
-
         return "".join(res)
