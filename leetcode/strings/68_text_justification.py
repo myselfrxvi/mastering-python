c5c18fc1c07ab61f9b4e28f384fae8696f4defdc
myselfrxvi <nooblikemeyo@gmail.com>
2026-09-21 17:43:24 +0530

    fix: complete text justification using space distribution join

--- leetcode/strings/68_text_justification.py
+++ leetcode/strings/68_text_justification.py
@@ -13,20 +13,17 @@ class Solution:
             else:
                 total_spaces = maxWidth - num_letters
                 if len(cur_line) == 1:
-                    res.append(cur_line[0] + " " * total_spaces)
+                    res.append(cur_line[0] + ' ' * total_spaces)
                 else:
                     num_gaps = len(cur_line) - 1
                     space_per_gap = total_spaces // num_gaps
                     extra_spaces = total_spaces % num_gaps
-                    line_parts = []
-                    for i in range(num_gaps):
-                        line_parts.append(cur_line[i])
-                        line_parts.append(" " * (space_per_gap + (1 if i < extra_spaces else 0)))
-                    line_parts.append(cur_line[-1])
-                    res.append("".join(line_parts))
+                    for i in range(extra_spaces):
+                        cur_line[i] += ' '
+                    res.append((' ' * space_per_gap).join(cur_line))
                 cur_line = [word]
                 num_letters = len(word)
 
-        last_line = " ".join(cur_line)
-        res.append(last_line + " " * (maxWidth - len(last_line)))
+        last_line = ' '.join(cur_line)
+        res.append(last_line + ' ' * (maxWidth - len(last_line)))
         return res
